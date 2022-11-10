68fc545464278e25f7e4606551d09b6c052ba56a
thisisrohan <rohankaushik@hotmail.com>
2022-11-11 00:48:36 +0100

    Wrote the residual GRUs, modified makedata.

---
+++ KS/tools/GRU_SingleStep_v8.py
@@ -0,0 +1,272 @@
+################################################################################
+# Regular residual GRU with skip connections, with uniform noise added to every#
+# input.                                                                       #
+#------------------------------------------------------------------------------#
+#                        Basic Network Architecture                            #
+#------------------------------------------------------------------------------#
+#                                                                     z1+d1    #
+#                                                  z1+d1                +d2    #
+#         __   z1   __   d1     z1+d1  __   d2       +d2  __   d3       +d3    #
+# u----->|__|----->|__|----->[+]----->|__|----->[+]----->|__|----->[+]----->   #
+#           \________________/ \________________/ \________________/           #
+#                                                                              #
+# Note here that you can only specify the number of layers and the number of   #
+# units in a layer, not the number of units in each layer individually. Also, a#
+# single layer network is the same as a regular GRU.                           #
+################################################################################
+
+import os
+import numpy as np
+from scipy import linalg
+
+import time as time
+
+import tensorflow as tf
+from tensorflow.keras import layers, losses, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras.regularizers import L2
+
+################################################################################
+#################################### LSTM V4 ###################################
+
+class learnable_state(layers.Layer):
+    def __init__(self, hidden_shape, b_regularizer=None, **kwargs):
+        super(learnable_state, self).__init__()
+        self.learnable_variable = self.add_weight(
+            name='learnable_variable',
+            shape=[1, hidden_shape],
+            initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1.0),
+            regularizer=b_regularizer,
+            trainable=True
+        )
+
+    def call(self, x):
+        batch_size = x.shape[0]
+        if batch_size == None:
+            batch_size = 1
+        return tf.tile(self.learnable_variable, [batch_size, 1])
+
+
+class uniform_noise(layers.Layer):
+    def __init__(self, mean=0.0, stddev=1e-4, **kwargs):
+        super(uniform_noise, self).__init__()
+        self.stddev = stddev
+        self.mean = mean
+
+    def call(self, x):
+        x = x + tf.random.uniform(shape=tf.shape(x), minval=self.mean-1.732051*self.stddev, maxval=self.mean+1.732051*self.stddev)
+        return x
+
+
+class RNN_GRU(Model):
+    """
+    Single-step GRU network that advances (in time) the latent space representation,
+    and has trainable initial states for the cell and memory states.
+    """
+    def __init__(
+            self, data_dim=None,
+            # in_steps=None,
+            # out_steps=None,
+            dt_rnn=None,
+            lambda_reg=0.0,
+            reg_name='L2',
+            rnn_layers_units=[3, 3, 3],
+            dense_layer_act_func='linear',
+            load_file=None,
+            mean=0.0,
+            stddev=1e-5):
+        
+        super(RNN_GRU, self).__init__()
+
+        self.load_file = load_file
+        # if self.load_file == None:
+        self.data_dim = data_dim
+        # self.out_steps = out_steps
+        self.dt_rnn = dt_rnn
+        self.lambda_reg = lambda_reg
+        self.reg_name = reg_name
+        self.rnn_layers_units = rnn_layers_units
+        self.dense_layer_act_func = dense_layer_act_func
+        self.mean = mean
+        self.stddev = stddev
+        if self.load_file is not None:
+            with open(load_file, 'r') as f:
+                lines = f.readlines()
+            load_dict = eval(lines[0])
+            if 'data_dim' in load_dict.keys():
+                self.data_dim = load_dict['data_dim']
+            # self.out_steps = load_dict['out_steps']
+            if 'dt_rnn' in load_dict.keys():
+                self.dt_rnn = load_dict['dt_rnn']
+            if 'lambda_reg' in load_dict.keys():
+                self.lambda_reg = load_dict['lambda_reg']
+            if 'reg_name' in load_dict.keys():
+                self.reg_name = load_dict['reg_name']
+            if 'rnn_layers_units' in load_dict.keys():
+                self.rnn_layers_units = load_dict['rnn_layers_units']
+            if 'dense_layer_act_func' in load_dict.keys():
+                self.dense_layer_act_func = load_dict['dense_layer_act_func']
+            if 'mean' in load_dict.keys():
+                self.mean = load_dict['mean']
+            if 'stddev' in load_dict.keys():
+                self.stddev = load_dict['stddev']
+        self.num_rnn_layers = len(self.rnn_layers_units)
+
+
+        ### the GRU network
+        if self.reg_name != None and self.lambda_reg != None and self.lambda_reg != 0:
+            reg = eval('tf.keras.regularizers.'+self.reg_name)
+            self.rnn_cells_list = [
+                layers.GRUCell(
+                    units=units,
+                    kernel_regularizer=reg(self.lambda_reg),
+                    bias_regularizer=reg(self.lambda_reg)
+                ) for units in self.rnn_layers_units
+            ]
+            
+            self.hidden_states_list = [learnable_state(hidden_shape=units, b_regularizer=reg(self.lambda_reg)) for units in self.rnn_layers_units]
+
+            self.dense = layers.Dense(
+                self.data_dim,
+                # kernel_initializer=tf.initializers.zeros(),
+                kernel_regularizer=reg(self.lambda_reg),
+                bias_regularizer=reg(self.lambda_reg)
+            )
+        else:
+            self.rnn_cells_list = [
+                layers.GRUCell(units=units) for units in self.rnn_layers_units
+            ]
+            
+            self.hidden_states_list = [learnable_state(hidden_shape=units) for units in self.rnn_layers_units]
+
+            self.dense = layers.Dense(
+                self.data_dim,
+                # kernel_initializer=tf.initializers.zeros(),
+            )
+
+
+        ### initializing weights
+        temp = tf.ones(shape=[1, 1, self.data_dim])
+        temp = self.predict(temp)
+
+        return
+
+    @tf.function
+    def call(self, inputs, training=None):
+
+        # inputs shape : (None, time_steps, data_dim)
+        out_steps = inputs.shape[1]
+
+        predictions_list = []
+        
+        ### Initialize the GRU state.
+        states_list = []
+        # first step
+        x = inputs[:, 0, :]
+        x = x + tf.random.uniform(shape=tf.shape(x), minval=self.mean-1.732051*self.stddev, maxval=self.mean+1.732051*self.stddev)
+        state1 = self.hidden_states_list[0](x, training=training)
+        prediction, *states = self.rnn_cells_list[0](
+            x,
+            states=state1,
+            training=training,
+        )
+        states_list.append(states[0])
+        x = prediction
+        for j in range(1, self.num_rnn_layers):
+            state1 = self.hidden_states_list[j](x, training=training)
+            prediction, *states = self.rnn_cells_list[j](
+                x,
+                states=state1,
+                training=training,
+            )
+            states_list.append(states[0])
+            x = prediction + x
+        prediction = self.dense(x, training=training)
+        predictions_list.append(prediction)
+
+        ### Remaining number of time-steps
+        for i in range(1, out_steps):
+            x = inputs[:, i, :]
+            x += tf.random.uniform(shape=tf.shape(x), minval=self.mean-1.732051*self.stddev, maxval=self.mean+1.732051*self.stddev)
+            state1 = states_list[0]
+            prediction, *states = self.rnn_cells_list[0](
+                x,
+                states=state1,
+                training=training,
+            )
+            states_list[0] = states[0]
+            x = prediction
+            for j in range(1, self.num_rnn_layers):
+                state1 = states_list[j]
+                prediction, *states = self.rnn_cells_list[j](
+                    x,
+                    states=state1,
+                    training=training,
+                )
+                states_list[j] = states[0]
+                x = prediction + x
+            prediction = self.dense(x, training=training)
+            predictions_list.append(prediction)
+
+        # predictions_list.shape => (time, batch, features)
+        predictions = tf.stack(predictions_list)
+        # predictions.shape => (batch, time, features)
+        predictions = tf.transpose(predictions, [1, 0, 2])
+
+        return predictions
+
+
+    def save_model_weights(self, file_name, H5=True):
+
+        # file_name = file_dir + '/' + file_name
+        # temp = tf.ones(shape=(1, self.data_dim,))
+        # temp = self.call(temp)
+
+        if H5 == True:
+            file_name += '.h5'
+        self.save_weights(file_name)
+        return
+    
+    def save_class_dict(self, file_name):
+        
+        class_dict = {
+            'data_dim':self.data_dim,
+            # 'out_steps':self.out_steps,
+            'dt_rnn':self.dt_rnn,
+            'lambda_reg':self.lambda_reg,
+            'reg_name':self.reg_name,
+            'rnn_layers_units':list(self.rnn_layers_units),
+            'dense_layer_act_func':self.dense_layer_act_func,
+            'load_file':self.load_file,
+            'mean':self.mean,
+            'stddev':self.stddev
+        }
+        with open(file_name, 'w') as f:
+            f.write(str(class_dict))
+            # s = '{\n'
+            # for entry in class_dict.keys():
+            #     s += '    '+str(entry)+':'+str(class_dict[entry])+',\n'
+            # s += '}'
+            # f.write(s)
+
+    def save_everything(self, file_name, H5=True):
+
+        ### saving class attributes
+        self.save_class_dict(file_name+'_class_dict.txt')
+
+        ### saving weights
+        self.save_model_weights(file_name+'_gru_weights', H5=H5)
+
+        return
+
+    def load_weights_from_file(self, file_name):
+
+        # temp = tf.ones(shape=(1, self.data_dim,))
+        # temp = self.call(temp)
+
+        self.load_weights(file_name)
+        return
+
+
+################################################################################
